14ae9d62d0357eb70fb111256c85ae7295384915
Alba Gómez Pérez <albagope@gmail.com>
2020-01-04 22:07:19 +0100

    add functionality all sections and refactor

--- main.js
+++ main.js
@@ -3,9 +3,11 @@
 const section1 = document.querySelector('.section-1');
 const section2 = document.querySelector('.section-2');
 const section3 = document.querySelector('.section-3');
-const info1 = document.querySelector('.image--1');
-const info2 = document.querySelector('.image--2');
-const info3 = document.querySelector('.image--3');
+section1.addEventListener('click', showInfo);
+section2.addEventListener('click', showInfo);
+section3.addEventListener('click', showInfo);
+
+
 let closeTransition = document.querySelector('.image--1').style;
 
 const otherDestinations = document.querySelector('.section--discover');
@@ -15,70 +17,53 @@ const destinySpace = document.querySelector('.new-Sections');
 
 
 function showInfo1(){
-    if(info1.classList.contains('hidden') === false){
+    // if(info1.classList.contains('hidden') === false){
         /*(function hola(){
             closeTransition.opacity = 1;
             (closeTransition.opacity-=.1)>0 ? closeTransition.display="none" : setTimeout(hola,60)
         })();*/
-        info1.classList.add('hidden');
+        // info1.classList.add('hidden');
 
-    }else{
-        info2.classList.add('hidden');
-        info3.classList.add('hidden');
-        info1.classList.remove('hidden');
+    // }else{
+        // info2.classList.add('hidden');
+        // info3.classList.add('hidden');
+        // info4.classList.add('hidden');
+        // info5.classList.add('hidden');
+        // info6.classList.add('hidden');
+        // info7.classList.add('hidden');
+        // info1.classList.remove('hidden');
         /*(function fade(){
             closeTransition.opacity=.1;
             closeTransition.display="block";
 
             (closeTransition.opacity+=.1)>1 ? closeTransition.color="red" : setTimeout(fade,60)
         })();*/
-    }
+    // }
 }
-section1.addEventListener('click', showInfo1);
 
-
-function showInfo2(){
-    if(info2.classList.contains('hidden') === false){
-        info2.classList.add('hidden');
-    }else{
-        info1.classList.add('hidden');
-        info3.classList.add('hidden');
-        info2.classList.remove('hidden');
-    }
+function showInfo(event){
+    document.querySelector('dd:not(.hidden)').classList.add('hidden');
+    event.currentTarget.nextElementSibling.classList.remove('hidden');
 }
-section2.addEventListener('click', showInfo2);
 
-
-function showInfo3(){
-    if(info3.classList.contains('hidden') === false){
-        info3.classList.add('hidden');
-    }else{
-        info1.classList.add('hidden');
-        info2.classList.add('hidden');
-        info3.classList.remove('hidden');
-    }
-}
-section3.addEventListener('click', showInfo3);
-
-// let transition = document.querySelector('.section-2').style;
-// transition.opacity = 1;
-// (function fade(){
-//     (transition.opacity-=.1)<0 ? transition.display="none" : setTimeout(fade,60)
-// })();
 
 //FETCH
-
 function showDestinations(){
     fetch(url)
         .then(response => response.json())
         .then(data => {
             places = data.Travelling;
-
             places.destinations.map((place) => {
                 destinySpace.innerHTML += generateHTMLDestiny(place);
             });
-        });
+            //@See: https://stackoverflow.com/a/36946222
+            places.destinations.map((place) => {
+                const section = document.querySelector('.section-' + place.id);
+                section.addEventListener('click', showInfo);
+            });
+    });
 }
+
 function generateHTMLDestiny(place){
     return `<dt class="section section-${place.id}">${place.destiny}<i class="fas fa-caret-down"></i></dt>
     <dd class="u-image image--${place.id} hidden">
