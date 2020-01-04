8c7de60d1aaa66056ce0b6aadccc3f4b5855f5cf
Alba Gómez Pérez <albagope@gmail.com>
2020-01-04 13:07:43 +0100

    add json with destinations info

--- main.js
+++ main.js
@@ -6,15 +6,27 @@ const section3 = document.querySelector('.section-3');
 const info1 = document.querySelector('.content-1');
 const info2 = document.querySelector('.content-2');
 const info3 = document.querySelector('.content-3');
+let closeTransition = document.querySelector('.content-1').style;
 
 
 function showInfo1(){
     if(info1.classList.contains('hidden') === false){
+        (function hola(){
+            closeTransition.opacity = 1;
+            (closeTransition.opacity-=.1)>0 ? closeTransition.display="none" : setTimeout(hola,60)
+        })();
         info1.classList.add('hidden');
+
     }else{
         info2.classList.add('hidden');
         info3.classList.add('hidden');
-        info1.classList.remove('hidden');
+        (function fade(){
+            closeTransition.opacity=.1;
+            closeTransition.display="block";
+            info1.classList.remove('hidden');
+            (closeTransition.opacity+=.1)>1 ? closeTransition.color="red" : setTimeout(fade,60)
+        })();
+        info1.classList.remove('colorHidden');
     }
 }
 section1.addEventListener('click', showInfo1);
@@ -43,9 +55,9 @@ function showInfo3(){
 }
 section3.addEventListener('click', showInfo3);
 
+// let transition = document.querySelector('.section-2').style;
+// transition.opacity = 1;
+// (function fade(){
+//     (transition.opacity-=.1)<0 ? transition.display="none" : setTimeout(fade,60)
+// })();
 
-    // let s = document.querySelector('.section-2').style;
-    // s.opacity = 1;
-    // (function fade(){
-    //     (s.opacity-=.1)<0 ? s.display="none" : setTimeout(fade,60)
-    // })();
