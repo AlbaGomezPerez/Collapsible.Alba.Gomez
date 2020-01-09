7b9860338b9f996be6c3b6a79e54c6bf90374633
Alba Gómez Pérez <albagope@gmail.com>
2020-01-09 21:41:39 +0100

    add transition and comments

--- main.js
+++ main.js
@@ -1,88 +1,72 @@
 'use strict';
 
+//
+const url = "./travelList.json";
+
+//
 const section1 = document.querySelector('.sections-place-title-1');
 const section2 = document.querySelector('.sections-place-title-2');
 const section3 = document.querySelector('.sections-place-title-3');
 const otherDestinations = document.querySelector('.section--discover');
-const finalSection = document.querySelector('.section--finally');
 section1.addEventListener('click', showInfo);
 section2.addEventListener('click', showInfo);
 section3.addEventListener('click', showInfo);
 
-const url = "./travelList.json";
-let places = '';
-const destinySpace = document.querySelector('.new-Sections');
-
-
-let closeTransition = document.querySelector('.sections-place-image--1').style;
-
-
-function showInfo1() {
-    // if(info1.classList.contains('hidden') === false){
-    /*(function hola(){
-        closeTransition.opacity = 1;
-        (closeTransition.opacity-=.1)>0 ? closeTransition.display="none" : setTimeout(hola,60)
-    })();*/
-    // info1.classList.add('hidden');
-
-    // }else{
-    /*(function fade(){
-        closeTransition.opacity=.1;
-        closeTransition.display="block";
-
-        (closeTransition.opacity+=.1)>1 ? closeTransition.color="red" : setTimeout(fade,60)
-    })();*/
-    // }
-}
-
-function closeInfo(event) {
-    if (event.currentTarget.classList.contains('u-hidden') === false) {
-        console.log('here');
-        event.currentTarget.classList.add('u-hidden')
-    } else {
-        showInfo()
-    }
-}
-
-function showInfo(event) {
-    document.querySelector('dd:not(.u-hidden)').classList.add('u-hidden');
-    event.currentTarget.nextElementSibling.classList.remove('u-hidden');
-}
-
-
-//FETCH
 /**
- * fetch to get info from travelList.json and paint the new html structure
+ *
+ * @param event
+ */
+function showInfo(event) {
+    let placeToClose = document.querySelector('dd:not(.u-place-close)');
+    placeToClose.classList.add('u-place-close');
+    placeToClose.classList.remove('u-place-open');
+
+    let placeSelected = event.currentTarget.nextElementSibling;
+
+    setTimeout(function() {
+        placeSelected.classList.remove('u-place-close');
+        placeSelected.classList.toggle('u-place-open');
+    }, 150);
+
+}
+
+/**
+ * Make a request to get info from travelList.json and render the new html structure
  */
 function showDestinations() {
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            places = data.Travelling;
+            let places = data.Travelling;
+            const destinySpace = document.querySelector('.new-Sections');
+
             places.destinations.map((place) => {
                 destinySpace.innerHTML += generateHTMLDestiny(place);
             });
             //@See: https://stackoverflow.com/a/36946222
-            //Second map because it's necessary wait the previously info
+            //Second map because it's necessary to wait to be rendered all html
             places.destinations.map((place) => {
                 const section = document.querySelector('.sections-place-title-' + place.id);
-                section.addEventListener('click', closeInfo);
+                section.addEventListener('click', showInfo);
             });
         });
     //Change the last part of sections. Remove the part which you can search places
     otherDestinations.classList.add('section--hidden');
+    const finalSection = document.querySelector('.section--finally');
     finalSection.classList.remove('section--hidden');
 }
 
 /**
- * Create html structure
+ * Create html structure for a place
+ * @param place an object that contains info from a specific destiny
  */
 function generateHTMLDestiny(place) {
     return `<dt class="u-place-title sections-place-title-${place.id}">${place.destiny}<i class="fas fa-caret-down"></i></dt>
-    <dd class="u-place-image sections-place-image--${place.id} u-hidden">
+    <dd class="u-place-image sections-place-image--${place.id} u-place-close">
         <p class="u-place-info sections-place-info--${place.id}">${place.description}
     </p>
     </dd>`;
 }
 
+//
 otherDestinations.addEventListener('click', showDestinations);
